64a36d9357af7100672d515352b594b26763507d
Alexandre Gomes <alegomes@gmail.com>
2023-12-27 10:25:32 -0300

    small refactor

--- walt-cli/src/main/kotlin/waltid/openbadgecredential/cli/IssueCmd.kt
+++ walt-cli/src/main/kotlin/waltid/openbadgecredential/cli/IssueCmd.kt
@@ -13,6 +13,7 @@ import id.walt.crypto.keys.KeyType
 import id.walt.crypto.keys.Key
 
 import id.walt.credentials.issuance.Issuer.baseIssue
+import id.walt.credentials.vc.vcs.W3CVC
 
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonElement
@@ -32,33 +33,12 @@ class IssueCmd : Runnable {
 
     override fun run() {
 
-        // Create VC document
         val entityIdentificationNumber = "1234"
         val issuingAuthorityId = "abcd"
         val proofType = "document"
         val proofLocation = "Brasilia/BR"
 
-        val credentialBuilder = CredentialBuilderType.W3CV2CredentialBuilder
-        val credentialSubject = mapOf(
-            "entityIdentification" to entityIdentificationNumber,
-            "issuingAuthority" to issuingAuthorityId,
-            "issuingCircumstances" to
-                    mapOf("proofType" to proofType,
-                          "locationType" to "physicalLocation",
-                          "location" to proofLocation)).toJsonObject()
-        val w3cCredential = CredentialBuilder(credentialBuilder)
-                            .apply { useCredentialSubject(credentialSubject) }
-                            .buildW3C()
-
-//        println("VC created: "+w3cCredential)
-
-        // Issue VC
-        // addContext("https://www.w3.org/ns/credentials/examples/v2") // [W3CV2 VC context, custom context]
-        // addType("MyCustomCredential") // [VerifiableCredential, MyCustomCredential]
-                               
-        // randomCredentialSubjectUUID() 
-
-        lateinit var key : Key
+        lateinit var key: Key
 
         runBlocking {
             key = LocalKey.generate(KeyType.Ed25519)
@@ -72,28 +52,67 @@ class IssueCmd : Runnable {
         val subjectDid = "did:key:sbj"    // DidService.create(DidMethod.key)
         val holderDid = "did:key:hodlr"
 
+        val w3cCredential = createVC(entityIdentificationNumber, issuingAuthorityId, proofType, proofLocation)
+
+        // Issue VC
+        val issuedVC = issueVC(entityIdentificationNumber, issuingAuthorityId, w3cCredential, issuerKey, issuerDid, subjectDid, holderDid)
+
+        println("Issued VC: $issuedVC")
+
+    }
+
+    private fun issueVC(entityIdentificationNumber: String,
+                        issuingAuthorityId: String,
+                        w3cCredential: W3CVC,
+                        issuerKey : Key,
+                        issuerDid : String,
+                        subjectDid : String,
+                        holderDid : String): String {
+
+        // addContext("https://www.w3.org/ns/credentials/examples/v2") // [W3CV2 VC context, custom context]
+        // addType("MyCustomCredential") // [VerifiableCredential, MyCustomCredential]
+        // randomCredentialSubjectUUID()
         // validFromNow()
         // validFor(90.days)
-
         // useStatusList2021Revocation("https://university.example/credentials/status/3", 94567)
 
         val dataOverwrites = mapOf("entityIdentification" to entityIdentificationNumber.toJsonElement())
         val dataUpdates = mapOf("issuingAuthority" to mapOf("issuingAuthority" to issuingAuthorityId.toJsonElement()))
 
+        var jwt : String = ""
         runBlocking {
-            val jwt = w3cCredential.baseIssue(
-                key = issuerKey,
-                did = issuerDid,
-                subject = holderDid,
-                dataOverwrites = emptyMap(),
-                dataUpdates = emptyMap(),
-                additionalJwtHeader = emptyMap(),
-                additionalJwtOptions = emptyMap(),
-        )
-
-            println("VC issued: " + jwt)
+            jwt = w3cCredential.baseIssue(
+                    key = issuerKey,
+                    did = issuerDid,
+                    subject = holderDid,
+                    dataOverwrites = emptyMap(),
+                    dataUpdates = emptyMap(),
+                    additionalJwtHeader = emptyMap(),
+                    additionalJwtOptions = emptyMap(),
+            )
         }
 
+        return jwt
+    }
 
+    private fun createVC(entityIdentificationNumber : String,
+                         issuingAuthorityId : String,
+                         proofType : String,
+                         proofLocation : String): W3CVC {
+        // Create VC document
+
+        val credentialBuilder = CredentialBuilderType.W3CV2CredentialBuilder
+        val credentialSubject = mapOf(
+                "entityIdentification" to entityIdentificationNumber,
+                "issuingAuthority" to issuingAuthorityId,
+                "issuingCircumstances" to
+                        mapOf("proofType" to proofType,
+                                "locationType" to "physicalLocation",
+                                "location" to proofLocation)).toJsonObject()
+        val w3cCredential = CredentialBuilder(credentialBuilder)
+                .apply { useCredentialSubject(credentialSubject) }
+                .buildW3C()
+
+        return w3cCredential
     }
 }
